d59446c59a1095b098fd1fec2e2849075a72acd1
Jamie Lentin <jm@lentin.co.uk>
2012-04-24 10:26:00 +0100

    kirkwood: Allow nand to be configured via. devicetree
    Add default configuration for NAND, to be enabled in your board config. Ensure
    clock gating is set appropriately when the NAND is enabled.
    
    Changes since last submission (V5) [Addressing comments by]:-
    * Add auxdata to ensure device name matches what the clk framework expects
    * Remove old code to manage clock gating
    
    Signed-off-by: Jamie Lentin <jm@lentin.co.uk>

--- arch/arm/mach-kirkwood/board-dt.c
+++ arch/arm/mach-kirkwood/board-dt.c
@@ -24,6 +24,11 @@ static struct of_device_id kirkwood_dt_match_table[] __initdata = {
 	{ }
 };
 
+struct of_dev_auxdata kirkwood_dt_auxdata_lookup[] __initdata = {
+	OF_DEV_AUXDATA("mrvl,orion-nand", KIRKWOOD_NAND_MEM_PHYS_BASE, "orion_nand", NULL),
+	{ }
+};
+
 static void __init kirkwood_dt_init(void)
 {
 	pr_info("Kirkwood: %s, TCLK=%d.\n", kirkwood_id(), kirkwood_tclk);
@@ -58,7 +63,8 @@ static void __init kirkwood_dt_init(void)
 	if (of_machine_is_compatible("dlink,dns-kirkwood"))
 		dnskw_init();
 
-	of_platform_populate(NULL, kirkwood_dt_match_table, NULL, NULL);
+	of_platform_populate(NULL, kirkwood_dt_match_table,
+			kirkwood_dt_auxdata_lookup, NULL);
 }
 
 static const char *kirkwood_dt_board_compat[] = {
